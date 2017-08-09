be0cabe1634d6861db147e13ccfdfa79ee5d1658
Shyam Mistry <shyammistry96@gmail.com>
2017-08-09 15:38:44 +0100

    Initial commit

---
+++ BslTranslator/BslAlphabet.cs
@@ -0,0 +1,134 @@
+﻿namespace Leap
+{
+    internal class BslAlphabet
+    {
+        public bool A(Hand left, Hand right)
+        {
+            return (AreWithin(left.Fingers[1].TipPosition, right.Fingers[0].TipPosition, 15)
+               && right.Fingers.FindAll(x => x.IsExtended).Count == 5
+               || AreWithin(left.Fingers[0].TipPosition, right.Fingers[1].TipPosition, 15)
+               && left.Fingers.FindAll(x => x.IsExtended).Count == 5);
+        }
+
+        public bool B(Hand left, Hand right)
+        {
+            return ((left.Fingers.FindAll(x => x.IsExtended == false).Count == 5) &&
+                   (right.Fingers.FindAll(x => x.IsExtended == false).Count == 5) &&
+                   AreWithin(left.Fingers[0].TipPosition, right.Fingers[0].TipPosition,25) &&
+                   AreWithin(left.Fingers[1].TipPosition, right.Fingers[1].TipPosition,25)) &&
+                  AreWithin(left.Fingers[2].TipPosition, right.Fingers[2].TipPosition,25) &&
+                  AreWithin(left.Fingers[3].TipPosition, right.Fingers[3].TipPosition,25) &&
+                  AreWithin(left.Fingers[4].TipPosition, right.Fingers[4].TipPosition,25);
+        }
+
+        public bool C(Hand left, Hand right)
+        {
+            return ((left.Fingers[0].IsExtended && left.Fingers[1].IsExtended && !left.Fingers[2].IsExtended
+                     && !left.Fingers[3].IsExtended && !left.Fingers[4].IsExtended &&
+                 !AreWithin(left.Fingers[1].TipPosition, right.Fingers[1].TipPosition, 15) 
+                    || (right.Fingers[0].IsExtended && right.Fingers[1].IsExtended && !right.Fingers[2].IsExtended
+                        && !right.Fingers[3].IsExtended && !right.Fingers[4].IsExtended) &&
+                    !AreWithin(left.Fingers[1].TipPosition, right.Fingers[1].TipPosition, 15) ));
+        }
+
+        public bool C(Hand hand)
+        {
+            return hand.Fingers[0].IsExtended && hand.Fingers[1].IsExtended && !hand.Fingers[2].IsExtended
+                   && !hand.Fingers[3].IsExtended && !hand.Fingers[4].IsExtended;
+        }
+
+        public bool D(Hand left, Hand right)
+        {
+            return (
+                !left.Fingers[0].IsExtended && left.Fingers[1].IsExtended
+                && !left.Fingers[2].IsExtended && !left.Fingers[3].IsExtended
+                && !left.Fingers[4].IsExtended && right.Fingers[0].IsExtended
+                && !right.Fingers[2].IsExtended && !right.Fingers[3].IsExtended && !right.Fingers[4].IsExtended
+                && AreWithin(left.Fingers[1].TipPosition, right.Fingers[1].TipPosition, 15)
+                || !right.Fingers[0].IsExtended && right.Fingers[1].IsExtended
+                && !right.Fingers[2].IsExtended && !right.Fingers[3].IsExtended
+                && !right.Fingers[4].IsExtended && left.Fingers[0].IsExtended
+                && !left.Fingers[2].IsExtended && !left.Fingers[3].IsExtended && !left.Fingers[4].IsExtended
+                && AreWithin(left.Fingers[1].TipPosition, left.Fingers[1].TipPosition, 15));
+            //     || left.Fingers.FindAll(x=>!x.IsExtended).Count ==5 && right.Fingers[0].IsExtended && right.Fingers[1].IsExtended && !right.Fingers[2].IsExtended
+            //    && !right.Fingers[3].IsExtended && !right.Fingers[4].IsExtended && AreTouching(right.Fingers[0].TipPosition, left.Fingers[1].bones[3].Center)
+            //     || right.Fingers.FindAll(x => !x.IsExtended).Count == 5 && left.Fingers[0].IsExtended && left.Fingers[1].IsExtended && !left.Fingers[2].IsExtended
+            //    && !left.Fingers[3].IsExtended && !left.Fingers[4].IsExtended && AreTouching(left.Fingers[0].TipPosition, right.Fingers[1].bones[3].Center));
+        }
+
+        public bool E(Hand left, Hand right)
+        {
+            return (left.Fingers[1].IsExtended && !left.Fingers[0].IsExtended
+                    && !left.Fingers[2].IsExtended && !left.Fingers[3].IsExtended
+                    && !left.Fingers[4].IsExtended && right.Fingers.TrueForAll(x => x.IsExtended)
+                    && AreWithin(left.Fingers[1].TipPosition, right.Fingers[1].TipPosition, 15)
+                    || right.Fingers[1].IsExtended && !right.Fingers[0].IsExtended
+                    && !right.Fingers[2].IsExtended && !right.Fingers[3].IsExtended
+                    && !right.Fingers[4].IsExtended && left.Fingers.TrueForAll(x => x.IsExtended)
+                    && AreWithin(left.Fingers[1].TipPosition, right.Fingers[1].TipPosition, 15));
+        }
+
+        public bool F(Hand left, Hand right)
+        {
+            return (left.Fingers[1].IsExtended && left.Fingers[2].IsExtended && left.Fingers[1].IsExtended &&
+                    left.Fingers[2].IsExtended
+                    && !left.Fingers[0].IsExtended && !left.Fingers[3].IsExtended && !left.Fingers[4].IsExtended
+                    && !right.Fingers[0].IsExtended && !right.Fingers[3].IsExtended && !right.Fingers[4].IsExtended)
+                    ;
+        }
+
+//        public bool X(Hand left, Hand right)
+//        {
+//            return (!left.Fingers[0].IsExtended && left.Fingers[1].IsExtended && !left.Fingers[2].IsExtended
+//                    && !left.Fingers[3].IsExtended && !left.Fingers[4].IsExtended
+//                    && !right.Fingers[0].IsExtended && right.Fingers[1].IsExtended && !right.Fingers[2].IsExtended
+//                    && !right.Fingers[3].IsExtended && !right.Fingers[4].IsExtended
+//                    && AreWithin(left.Fingers[1].bones[2].Center, right.Fingers[1].bones[2].Center, 15));
+//        }
+        public bool G(Hand hand)
+        {
+            return (hand.Fingers.FindAll(x => !x.IsExtended).Count == 5);
+        }
+
+        public bool H(Hand left, Hand right)
+        {
+            return right.Fingers.TrueForAll(x => x.IsExtended) && left.Fingers.TrueForAll(x => x.IsExtended) 
+                && (left.PalmNormal.y<=1 &&left.PalmNormal.y>=0.7&&right.PalmNormal.y>=-1&&right.PalmNormal.y<=-0.7) 
+                || (right.PalmNormal.y <= 1 && right.PalmNormal.y >= 0.7 && left.PalmNormal.y >= -1 && left.PalmNormal.y <= -0.7);
+        }
+        public bool I(Hand left, Hand right)
+        {
+            return (AreWithin(left.Fingers[1].TipPosition, right.Fingers[2].TipPosition, 15)
+              && right.Fingers.FindAll(x => x.IsExtended).Count == 5
+              || AreWithin(left.Fingers[2].TipPosition, right.Fingers[1].TipPosition, 15)
+              && left.Fingers.FindAll(x => x.IsExtended).Count == 5);
+        }
+
+
+//        public bool K(Hand left, Hand right)
+//        {
+//            
+//        }
+
+        public bool O(Hand left, Hand right)
+        {
+            return (AreWithin(left.Fingers[1].TipPosition, right.Fingers[3].TipPosition, 15)
+              && right.Fingers.FindAll(x => x.IsExtended).Count == 5
+              || AreWithin(left.Fingers[3].TipPosition, right.Fingers[1].TipPosition,15)
+              && left.Fingers.FindAll(x => x.IsExtended).Count == 5);
+        }
+        public bool U(Hand left, Hand right)
+        {
+            return (AreWithin(left.Fingers[1].TipPosition, right.Fingers[4].TipPosition,15)
+              && right.Fingers.FindAll(x => x.IsExtended).Count == 5
+              || AreWithin(left.Fingers[4].TipPosition, right.Fingers[1].TipPosition,15)
+              && left.Fingers.FindAll(x => x.IsExtended).Count == 5);
+        }
+
+
+        private bool AreWithin(Vector finger1, Vector finger2, int distance)
+        {
+            return (finger1 - finger2).Magnitude < distance;
+        }
+    }
+}
---
+++ BslTranslator/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Leap;
+
+namespace Leap
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            var controller = new Controller();
+            var listener = new SampleListener();
+            controller.Connect += listener.OnServiceConnect;
+            controller.Device += listener.OnConnect;
+            controller.FrameReady += listener.OnFrame;
+
+            // Keep this process running until Enter is pressed
+            Console.WriteLine("Press Enter to quit...");
+            Console.ReadLine();
+
+            controller.StopConnection();
+            controller.Dispose();
+
+        }
+
+    }
+    class SampleListener
+    {
+        BslAlphabet alphabet = new BslAlphabet();
+        public void OnServiceConnect(object sender, ConnectionEventArgs args)
+        {
+            Console.WriteLine("Service Connected");
+        }
+
+        public void OnConnect(object sender, DeviceEventArgs args)
+        {
+            Console.WriteLine("Connected");
+        }
+
+        public void OnFrame(object sender, FrameEventArgs args)
+        {
+            // Get the most recent frame and report some basic information
+            Frame frame = args.frame;
+
+            //            Console.WriteLine(
+            //                "Frame id: {0}, timestamp: {1}, hands: {2}",
+            //                frame.Id, frame.Timestamp, frame.Hands.Count
+            //            );
+
+            foreach (Hand hand in frame.Hands)
+            {
+                //                Console.WriteLine("  Hand id: {0}, palm position: {1}, fingers: {2}",
+                //                    hand.Id, hand.PalmPosition, hand.Fingers.Count);
+                // Get the hand's normal vector and direction
+                Vector normal = hand.PalmNormal;
+                if (frame.Hands.Count == 2)
+                {
+
+
+
+                    if (alphabet.A(frame.Hands[0], frame.Hands[1]))
+                    {
+                        Console.WriteLine("A");
+                    }
+                    else if (alphabet.B(frame.Hands[0], frame.Hands[1]))
+                    {
+                        Console.WriteLine("B");
+                    }
+                    else if (alphabet.E(frame.Hands[0], frame.Hands[1]))
+                    {
+                        Console.WriteLine("E");
+                    }
+                    else if (alphabet.D(frame.Hands[0], frame.Hands[1]))
+                    {
+                        Console.WriteLine("D");
+                    }
+
+                    else if (alphabet.F(frame.Hands[0], frame.Hands[1]))
+                    {
+                        Console.WriteLine("F");
+                    }
+                    else if (alphabet.I(frame.Hands[0], frame.Hands[1]))
+                    {
+                        Console.WriteLine("I");
+                    }
+                    else if (alphabet.O(frame.Hands[0], frame.Hands[1]))
+                    {
+                        Console.WriteLine("O");
+                    }
+                    else if (alphabet.U(frame.Hands[0], frame.Hands[1]))
+                    {
+                        Console.WriteLine("U");
+                    }
+                    else if (alphabet.C(frame.Hands[0], frame.Hands[1]))
+                    {
+                        Console.WriteLine("C");
+                    }
+                    else if (alphabet.H(frame.Hands[0], frame.Hands[1]))
+                    {
+                        Console.WriteLine("H");
+                    }
+                }
+                if (frame.Hands.Count == 1)
+                {
+                    if (alphabet.G(frame.Hands[0]))
+                    {
+                        Console.WriteLine("G");
+                    }
+                    if (alphabet.C(frame.Hands[0]))
+                    {
+                        Console.WriteLine("C");
+                    }
+                    // Console.WriteLine(hand.PalmNormal);
+                }
+                Vector direction = hand.Direction;
+
+              
+            }
+        }
+    }
+}
